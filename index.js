d2a3af0195ca90f0d189b2b3b92b7f5f372c3d06
Austin <austin.yau@gmail.com>
2019-09-27 11:47:48 -0700

    set up server to hit LinkedIn Word API

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require('express');
+const cors = require('cors');
+const axios = require('axios');
+
+
+const app = express();
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cors());
+
+app.get('/words', async function(req, res) {
+  const params = req.query;
+  const response = await axios({
+    baseURL: "http://app.linkedin-reach.io/",
+    method: "get",
+    url: "/words",
+    params 
+  });
+
+  let words = response.data.split("\n");
+  let data = {
+    words
+  };
+
+  return res.json(data);
+});
+
+app.listen(3005, function() {
+  console.log("App on port 3005");
+});
